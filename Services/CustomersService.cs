d92e481ca821dd023a9efb50cfb461e57fede5fa
bagdenius <bagdensparrow@gmail.com>
2022-05-04 21:10:11 +0300

    Added services

---
+++ Services/CustomersService.cs
@@ -0,0 +1,41 @@
+﻿using Domains;
+using Services.Abstract;
+using UoW.Abstract;
+
+namespace Services
+{
+    public class CustomersService : IService<Customer>
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public CustomersService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void Add(Customer domain)
+        {
+            _unitOfWork.Customers.Add(domain);
+        }
+
+        public void Update(Customer domain)
+        {
+            _unitOfWork.Customers.Update(domain);
+        }
+
+        public void Remove(Customer domain)
+        {
+            _unitOfWork.Customers.Remove(domain);
+        }
+
+        public Customer GetById(int id)
+        {
+            return _unitOfWork.Customers.GetById(id);
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            return _unitOfWork.Customers.GetAll();
+        }
+    }
+}
---
+++ Services/RoomsService.cs
@@ -0,0 +1,41 @@
+﻿using Domains;
+using Services.Abstract;
+using UoW.Abstract;
+
+namespace Services
+{
+    public class RoomsService : IService<Room>
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public RoomsService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void Add(Room domain)
+        {
+            _unitOfWork.Rooms.Add(domain);
+        }
+
+        public void Update(Room domain)
+        {
+            _unitOfWork.Rooms.Update(domain);
+        }
+
+        public void Remove(Room domain)
+        {
+            _unitOfWork.Rooms.Remove(domain);
+        }
+
+        public Room GetById(int id)
+        {
+            return _unitOfWork.Rooms.GetById(id);
+        }
+
+        public IEnumerable<Room> GetAll()
+        {
+            return _unitOfWork.Rooms.GetAll();
+        }
+    }
+}
