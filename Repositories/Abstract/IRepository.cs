040e7a5291923bd82af46b96777e5fbdb61b0a55
bagdenius <bagdensparrow@gmail.com>
2022-05-04 18:08:12 +0300

    Added generic repository interface

--- Repositories/Abstract/IRepository.cs
+++ Repositories/Abstract/IRepository.cs
@@ -2,6 +2,10 @@
 {
     internal interface IRepository<TEntity> where TEntity : class
     {
-
+        public void Add(TEntity entity);
+        public void Update(TEntity entity);
+        public void Delete(TEntity entity);
+        public TEntity GetById(int id);
+        public IEnumerable<TEntity> GetAll();
     }
 }
