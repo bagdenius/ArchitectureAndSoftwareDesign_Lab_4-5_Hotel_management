61d20f1a11d30fbb1b4e9fc8934ecc67511a1c0d
bagdenius <bagdensparrow@gmail.com>
2022-05-04 17:56:10 +0300

    Added Hotel entity

--- Entities/Abstract/IHotelEntity.cs
+++ Entities/Abstract/IHotelEntity.cs
@@ -2,6 +2,13 @@
 {
     internal interface IHotelEntity
     {
-
+        public int Id { get; set; }
+        public List<RoomEntity> Rooms { get; set; }
+        public string Name { get; set; }
+        public int Stars { get; set; }
+        public int NumberOfRooms { get; set; }
+        public int NumberOfFloors { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
     }
 }
--- Entities/HotelEntity.cs
+++ Entities/HotelEntity.cs
@@ -1,7 +1,16 @@
-﻿namespace Entities
-{
-    internal class HotelEntity
-    {
+﻿using Entities.Abstract;
 
+namespace Entities
+{
+    internal class HotelEntity : IHotelEntity
+    {
+        public int Id { get; set; }
+        public List<RoomEntity> Rooms { get; set; }
+        public string Name { get; set; }
+        public int Stars { get; set; }
+        public int NumberOfRooms { get; set; }
+        public int NumberOfFloors { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
     }
 }
